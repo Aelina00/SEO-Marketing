b66d51485049e3428d291c357b1a2e3472c71d7b
castaway <fltes.shx@gmail.com>
2024-02-26 14:51:54 +0600

    added some new

--- core.js
+++ core.js
@@ -1,38 +1,35 @@
-
 $(document).ready(function () {
-    //responsive menu toggle
-    $("#menutoggle").click(function () {
-        $('.xs-menu').toggleClass('displaynone');
-
-    });
-    //add active class on menu
-    $('ul li').click(function (e) {
-        e.preventDefault();
-        $('li').removeClass('active');
-        $(this).addClass('active');
-    });
-    //drop down menu	
-    $(".drop-down").hover(function () {
-        $('.mega-menu').addClass('display-on');
-    });
-    $(".drop-down").mouseleave(function () {
-        $('.mega-menu').removeClass('display-on');
-    });
-
+  //responsive menu toggle
+  $("#menutoggle").click(function () {
+    $(".xs-menu").toggleClass("displaynone");
+  });
+  //add active class on menu
+  $("ul li").click(function (e) {
+    e.preventDefault();
+    $("li").removeClass("active");
+    $(this).addClass("active");
+  });
+  //drop down menu
+  $(".drop-down").hover(function () {
+    $(".mega-menu").addClass("display-on");
+  });
+  $(".drop-down").mouseleave(function () {
+    $(".mega-menu").removeClass("display-on");
+  });
 });
 
 function openTab(evt, tabName) {
-	var i, tabcontent, tablinks
-	tabcontent = document.getElementsByClassName('tab-content')
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].className = tabcontent[i].className.replace(' active', '')
-	}
-	tablinks = document.getElementsByClassName('tab')
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(' active', '')
-	}
-	document.getElementById(tabName).className += ' active'
-	evt.currentTarget.className += ' active'
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].className = tabcontent[i].className.replace(" active", "");
+  }
+  tablinks = document.getElementsByClassName("tab");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).className += " active";
+  evt.currentTarget.className += " active";
 }
 
 const EL_play = document.querySelector("#play");
@@ -40,11 +37,40 @@ const EL_videoContainer = document.querySelector("#video-container");
 const EL_video = document.querySelector("#video");
 
 EL_play.addEventListener("click", () => {
-    EL_videoContainer.classList.toggle("u-none");
-    const isPaused = EL_video.paused;
-    EL_video[isPaused ? "play" : "pause"]();
+  EL_videoContainer.classList.toggle("u-none");
+  const isPaused = EL_video.paused;
+  EL_video[isPaused ? "play" : "pause"]();
 });
 function enqueue_jquery() {
-    wp_enqueue_script('jquery');
+  wp_enqueue_script("jquery");
 }
-add_action('wp_enqueue_scripts', 'enqueue_jquery'); 
+add_action("wp_enqueue_scripts", "enqueue_jquery");
+
+const slider = document.querySelector(".slider");
+const items = slider.querySelectorAll(".slider__item");
+const prev = slider.querySelector(".slider__prev");
+const next = slider.querySelector(".slider__next");
+let currentIndex = 0;
+
+function showSlide(index) {
+  items.forEach((item, i) => {
+    item.style.opacity = i === index ? 1 : 0;
+  });
+  currentIndex = index;
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % items.length;
+  showSlide(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex = (currentIndex - 1 + items.length) % items.length;
+  showSlide(currentIndex);
+}
+
+prev.addEventListener("click", prevSlide);
+next.addEventListener("click", nextSlide);
+
+showSlide(currentIndex);
+setInterval(nextSlide, 5000);
